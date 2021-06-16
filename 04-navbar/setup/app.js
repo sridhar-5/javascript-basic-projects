e5cc518bc82ce0094e8d77c304b9957c04a851ef
sridhar-5 <kv.sridharsai@gmail.com>
2021-06-16 13:42:02 +0530

    completed navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,17 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const togglebutton = document.querySelector(".nav-toggle");
+
+const links = document.querySelector(".links");
+
+togglebutton.addEventListener("click", NavBarActive);
+
+function NavBarActive() {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  }
+}
